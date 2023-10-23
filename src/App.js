4e040dfd7886ac201f587fa0873a58abcfc58423
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-10-23 17:59:40 +0300

    feat(app.js): added the value prop and onSquareClick props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className='square' onClick={handleClick}>
+    <button className='square' onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquare = square.slice();
+    nextSquare[i] = "X";
+    setSquare(nextSquare);
+  }
   return (
     <>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
